9e972d78c22486806e3c57d3070554f07bc7d87e
fernandes.mvd@gmail.com <Marcos@DESKTOP-PM7DCU7.bbrouter>
2025-08-26 23:19:15 -0300

    Tarefa concluida

---
+++ src/DivisorDeNotas/Notas.java
@@ -0,0 +1,27 @@
+package DivisorDeNotas;
+
+public class Notas {
+	
+	public static void main(String [] args) {
+		sistemasDeNotas ();
+	}
+
+	private static void sistemasDeNotas() {
+		int nota1 = 5;
+		int nota2 = 8;
+		int nota3 = 9;
+		int nota4 = 10;
+		
+		int resultado = nota1 / 4;
+		System.out.println(resultado);
+		int resultado2 = nota2 / 4;
+		System.out.println(resultado2);
+		int resultado3 = nota3 / 4;
+		System.out.println(resultado3);
+		int resultado4 = nota4 / 4;
+		System.out.println(resultado4);
+		
+				
+		
+	}
+}
